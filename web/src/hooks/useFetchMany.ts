a9a7d8218fa9593753e721d7d23f0d09a3623a44
Raul Rosá <raul.hmetal@gmail.com>
2020-10-17 12:30:43 -0300

    Update: Quick fixes

--- web/src/hooks/useFetchMany.ts
+++ web/src/hooks/useFetchMany.ts
@@ -22,7 +22,7 @@ export function useFetchMany<T = unknown>(URL: string): IFetchReturn<T> {
     if (result) setData(result);
 
     setLoading(false);
-  }, [URL]);
+  }, [URL, apiGet]);
 
   useEffect(() => {
     fetchFromApi();
